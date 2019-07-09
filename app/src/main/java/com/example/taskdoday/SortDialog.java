8173aacf8a5516c163faf0213fd9c134ff2dc163
DavidCBeach <dbeach@csumb.edu>
2019-07-08 22:40:25 -0700

    adding stats, delete multiline, new look

---
+++ app/src/main/java/com/example/taskdoday/SortDialog.java
@@ -0,0 +1,42 @@
+package com.example.taskdoday;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class SortDialog extends DialogFragment {
+
+
+
+
+    public SortDialog() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_sort, container);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+    public void dismisser(){
+        dismiss();
+    }
+
+}
--- app/src/main/java/com/example/taskdoday/StatsActivity.java
+++ app/src/main/java/com/example/taskdoday/StatsActivity.java
@@ -1,14 +1,110 @@
 package com.example.taskdoday;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class StatsActivity extends AppCompatActivity {
-
+    FeedReaderDbHelper dbHelper;
+    Calendar calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        setTitle("Stats");
+        setTitle("Statistics");
+        dbHelper = new FeedReaderDbHelper(getApplicationContext());
+        calendar = Calendar.getInstance();
+        percentRead();
+
+
+    }
+    private void percentRead(){
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        SimpleDateFormat mdformat = new SimpleDateFormat("MM");
+        String month =  mdformat.format(calendar.getTime());
+        int imonth = Integer.parseInt(month);
+        SimpleDateFormat dmdformat = new SimpleDateFormat("dd");
+        String day =  dmdformat.format(calendar.getTime());
+        int iday = Integer.parseInt(day);
+        SimpleDateFormat ymdformat = new SimpleDateFormat("yyyy");
+        String year =  ymdformat.format(calendar.getTime());
+        int iyear= Integer.parseInt(year);
+
+        calendar.set(iyear,imonth,iday,0,0);
+
+
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_CONTENT,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_STATUS,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_DATE,
+
+        };
+        String sortOrder = FeedReaderContract.FeedEntry.COLUMN_NAME_STATUS + " ASC";
+        // Filter results WHERE "title" = 'My Title'
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_DATE + " < ?";
+        //String[] selectionArgs = { "My Title" };
+        String[] selectionArgs = {Long.toString(calendar.getTimeInMillis())};
+
+        // How you want the results sorted in the resulting Cursor
+
+
+        Cursor cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+
+        ArrayList<Integer> list1 = new ArrayList();
+        ArrayList<Integer> list0= new ArrayList();
+        while(cursor.moveToNext()) {
+            int status = cursor.getInt(
+                    cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_STATUS));
+            if(status == 0){
+                list0.add(status);
+            } else {
+                list1.add(status);
+            }
+
+
+        }
+        if(!(list0.isEmpty() && list1.isEmpty())){
+            TextView nostats = findViewById(R.id.nostats);
+            nostats.setVisibility(View.GONE);
+            System.out.println(list0.size());
+            System.out.println(list1.size());
+
+            Double percent = Double.valueOf(list1.size())/Double.valueOf(list1.size()+list0.size()) * 1000.0;
+            percent = Math.floor(percent)/10.0;
+            System.out.println(percent);
+            TextView percentview = findViewById(R.id.percent);
+            percentview.setText(percent.toString() + "%");
+        }
+
+
+
+        cursor.close();
+
+        db.close();
+
     }
 }
